cc7273978335fba6d1a8bc39c2e3ee26c9dbf04f
carolinehanaa <carolinehana28@gmail.com>
2022-10-20 11:56:21 -0700

    push 2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-﻿//Caroline Hana
-//10-19-2022
-/*Ask your name
-Ask what time you woke up
-Print out "Your name woke up today at time"
-Peer Review- 
-*/
-
+﻿
 Console.Clear();
 string? playAgain = "yes";
 while (playAgain == "yes" )
